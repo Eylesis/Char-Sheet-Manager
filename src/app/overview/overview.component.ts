1503b7aeef52436089b6a568f8b7bd5ce3dffa7c
Eylesis <kyntenslocum@gmail.com>
2018-11-21 22:15:47 -0800

    Fixed persistent data loading on components that were init already

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { CharacterService }  from '../character.service';
 import { Character } from '../character';
 import { ActivatedRoute } from '@angular/router';
@@ -28,59 +28,56 @@ export class OverviewComponent implements OnInit {
   
     ngOnInit(): void {
       this.getCharacter();
-      this.abiNames = ["Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom", "Charisma"];
-      this.abiScores = [];
-      this.abiMods = [];
-      this.modDecorator = [];
-      this.saveDecorator = [];
-      this.savingThrows = [];
-      this.profButton = [];
-      
-      var temp = this.character.stats;
-      this.charStats = [temp.ac, temp.prof, temp.speed, temp.init];
-      this.statName = ["Armor Class", "Proficiency Bonus", "Speed", "Initiative"];
-
-      
-      var index = 0;
-      
-    
-
-      for(let key in this.character.abiScores) 
-      {
-          let value = this.character.abiScores[key];
-          this.abiScores[index] = value;
-          this.abiMods[index] = Math.floor((value -10) / 2);
-          this.savingThrows[index] = this.character.savingThrows[key + "Save"];
-          if (this.abiMods[index] >= 0)
-            this.modDecorator[index] = "+";
-          else
-            this.modDecorator[index] = "";
-
-          if (this.savingThrows[index] == 1)
-          {
-            this.savingThrows[index] = this.abiMods[index] + this.character.stats.prof;
-            this.profButton[index] = "radio_button_checked";
-          }
-          else
-          {
-            this.savingThrows[index] = this.abiMods[index];
-            this.profButton[index] = "radio_button_unchecked";
-          }
-          
-          
-            if (this.savingThrows[index] >= 0)
-            this.saveDecorator[index] = "+";
-          else
-            this.saveDecorator[index] = "";
-          
-          index++;
-      }
     }
-  
+
     getCharacter(): void {
       this.route.params.subscribe( params => {
         const id = +params['id'];
         this.characterService.getCharacter(id).subscribe(character => this.character = character);
+      
+        this.abiNames = ["Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom", "Charisma"];
+        this.abiScores = [];
+        this.abiMods = [];
+        this.modDecorator = [];
+        this.saveDecorator = [];
+        this.savingThrows = [];
+        this.profButton = [];
+        
+        var temp = this.character.stats;
+        this.charStats = [temp.ac, temp.prof, temp.speed, temp.init];
+        this.statName = ["Armor Class", "Proficiency Bonus", "Speed", "Initiative"];
+      
+        var index = 0;
+        for(let key in this.character.abiScores) 
+        {
+            let value = this.character.abiScores[key];
+            this.abiScores[index] = value;
+            this.abiMods[index] = Math.floor((value -10) / 2);
+            this.savingThrows[index] = this.character.savingThrows[key + "Save"];
+            if (this.abiMods[index] >= 0)
+              this.modDecorator[index] = "+";
+            else
+              this.modDecorator[index] = "";
+
+            if (this.savingThrows[index] == 1)
+            {
+              this.savingThrows[index] = this.abiMods[index] + this.character.stats.prof;
+              this.profButton[index] = "radio_button_checked";
+            }
+            else
+            {
+              this.savingThrows[index] = this.abiMods[index];
+              this.profButton[index] = "radio_button_unchecked";
+            }
+            
+            
+              if (this.savingThrows[index] >= 0)
+              this.saveDecorator[index] = "+";
+            else
+              this.saveDecorator[index] = "";
+            
+            index++;
+        }
       })
     }
 }
